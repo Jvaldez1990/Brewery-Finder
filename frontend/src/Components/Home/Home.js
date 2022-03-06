1236cd911b5764f9070b106489b71d004f237d6e
Jose V <Josejoeyvaldez@gmail.com>
2022-03-06 14:52:59 -0500

    Some clean up on Home to get things to show

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,17 +1,15 @@
-// import { Component } from "react";
-// import { connect } from "react-redux";
-// import { Link } from "react-router-dom";
-// import { fetchBreweries } from "../../Redux/actionCreators";
+import { Component } from "react";
+import { connect } from "react-redux";
+import { fetchBreweries } from "../../Redux/actionCreators";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import BreweryList from "../Login/BreweryList";
 
-// const mapStateToProps = (state) => {
-//   return {
-//     breweries: state.breweries,
-//   };
-// };
-
-// import {Link} from 'react-router-dom'
-// import { useState } from 'react';
-// import BreweryList from '../Login/BreweryList';
+const mapStateToProps = (state) => {
+  return {
+    breweries: state.breweries,
+  };
+};
 
 const Home = () => {
   const [breweries, setBreweries] = useState([
@@ -58,17 +56,17 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
-class Home extends Component {
-  componentDidMount() {
-    this.props.fetchBreweries();
-  }
+// class Home extends Component {
+//   componentDidMount() {
+//     this.props.fetchBreweries();
+//   }
 
-  render() {
-    const menu = this.props.breweries.map((brewery) => {
-      return <div>{brewery}</div>;
-    });
-    return <div>{menu}</div>;
-  }
-}
+//   render() {
+//     const menu = this.props.breweries.map((brewery) => {
+//       return <div>{brewery}</div>;
+//     });
+//     return <div>{menu}</div>;
+//   }
+// }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
