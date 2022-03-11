e10f0579cfb77331348ca4545bc721ec76cc6c3e
smithg <smith.garcia345@gmail.com>
2022-03-11 14:40:33 -0800

    addbeer is done just need to add redux

--- frontend/src/Components/add/update/AddBeer.jsx
+++ frontend/src/Components/add/update/AddBeer.jsx
@@ -1,59 +1,56 @@
 import { useState } from 'react'
 import Card from '../../../Shared/Card'
 import RatingSelect from '../RatingSelect'
+import { Form, Field } from 'react-final-form'
 import { connect } from 'react-redux'
 import { addBeer } from '../../../Redux/actionCreators'
 
 function AddPost() {
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-    const [beerRating, setBeerRating] = useState()
 
-    const onSubmit = (e) => {
-      e.preventDefault() 
-      
-      if(!title) {
-        alert('Please add a brewery')
-        return
-      }else if(!body){
-        alert('Please add a review')
-        return
-      }else if(!title && !body) {
-        alert('please add a Brewrey and Review')
-        return
-      }
+  const onSubmit = (e) => {
+     debugger
+  }
 
-
-      setBeerRating()
-      setBody('')
-      setTitle('')
+  const validate = (e) => {
+    const errors = {
     }
 
-  return (
-    <Card>
-    <form onSubmit={onSubmit}>
-      <h2>How would rate our Service?</h2>
-      <RatingSelect select={setBeerRating} selected={beerRating} />
-        <div className='form-control'>
-            <label>Add Beer</label>
-            <input type="text" 
-            placeholder='Add Beer'
-            value={title}
-            />
-            </div>
-            <div className='form-control'>
-            <label>Review: </label>
-            <textarea 
-            name="body" 
-            value={body}
-            cols="30" 
-            rows="10" />
-        </div>
+    if(e.Des && e.Des.length < 4 ){
+      errors.Des = 'Please add Beer description.'
+    }
 
-        <input type='submit' value='Save Beer'/>
-    </form>
-    </Card>
+    return errors;
+  }
+
+  return(
+    <Form
+    onSubmit={onSubmit}
+    validate={validate}
+    render={({ handleSubmit }) => ( 
+        <form onSubmit={handleSubmit}>
+         <div>
+            <label>Beer Name</label>
+            <Field name="Name" component="input" placeholder="Beer Name" />
+         </div>
+         <div>
+           <label>Description</label>
+          <Field
+              name="Des"
+              render={({ input, meta }) => (
+                <div>
+                  <textarea {...input} />
+                  {meta.touched && meta.error && <div>{meta.error}</div>}
+                </div>
+              )}
+          />
+         </div>
+         <br />
+         <button type="submit">Submit</button>
+        </form>
+    )
+  } />
   )
+  
 }
 
-export default connect(null, { addBeer })(AddPost)
+export default connect()(AddPost)
